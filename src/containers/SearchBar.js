ceef86c35e063bf271162e0244b6ff25f82acf5c
Frederic Rey <Frederic.Rey.Pro@gmail.com>
2016-07-16 17:04:17 +0100

    - SeachBar onSubmit handler in progress

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -5,21 +5,51 @@ const propTypes = {
 };
 
 class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Init state
+    this.state = {
+      term: '',
+    };
+
+    // Handlers bindings
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({
+      term: e.target.value,
+    });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <form className="form-inline">
+      <form
+        onSubmit={this.onFormSubmit}
+        className="form-inline">
         <div className="form-group">
           <div className="input-group">
             <div className="input-group-addon">
               <span className="glyphicon glyphicon-search"></span>
             </div>
             <input
-              type="text"
               className="form-control"
-              placeholder="Search for a city"/>
+              type="text"
+              name="city"
+              placeholder="Search for a city"
+              value={this.state.term}
+              onChange={this.onInputChange}/>
           </div>
         </div>
-        <button className="btn btn-primary">Search</button>
+        <button
+          role="submit"
+          className="btn btn-secondary">Search</button>
       </form>
     );
   }
